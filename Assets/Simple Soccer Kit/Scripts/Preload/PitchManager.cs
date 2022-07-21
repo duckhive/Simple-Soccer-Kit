6152dadab97fd805331674b4776bdd1d80d90cbf
duq games <99378893+duqgames@users.noreply.github.com>
2022-07-21 11:01:02 -0400

    Adjusted Player Positions

--- Assets/Simple Soccer Kit/Scripts/Preload/PitchManager.cs
+++ Assets/Simple Soccer Kit/Scripts/Preload/PitchManager.cs
@@ -34,47 +34,47 @@ public class PitchManager : MonoBehaviour
     {
         GameManager.Instance.allPlayers[0].homeZone = pitchZones[13];
         GameManager.Instance.allPlayers[1].homeZone = pitchZones[14];
-        GameManager.Instance.allPlayers[2].homeZone = pitchZones[9];
-        GameManager.Instance.allPlayers[3].homeZone = pitchZones[10];
+        GameManager.Instance.allPlayers[2].homeZone = pitchZones[8];
+        GameManager.Instance.allPlayers[3].homeZone = pitchZones[11];
         GameManager.Instance.allPlayers[4].homeZone = pitchZones[22];
         GameManager.Instance.allPlayers[5].homeZone = pitchZones[21];
-        GameManager.Instance.allPlayers[6].homeZone = pitchZones[26];
-        GameManager.Instance.allPlayers[7].homeZone = pitchZones[25];
-    }
-
-    private void SetPlayerAttackZones()
-    {
-        GameManager.Instance.allPlayers[0].attackZone = pitchZones[30];
-        GameManager.Instance.allPlayers[1].attackZone = pitchZones[24];
-        GameManager.Instance.allPlayers[2].attackZone = pitchZones[27];
-        GameManager.Instance.allPlayers[3].attackZone = pitchZones[21];
-        GameManager.Instance.allPlayers[4].attackZone = pitchZones[5];
-        GameManager.Instance.allPlayers[5].attackZone = pitchZones[11];
-        GameManager.Instance.allPlayers[6].attackZone = pitchZones[8];
-        GameManager.Instance.allPlayers[7].attackZone = pitchZones[14];
-    }
-
-    private void SetPlayerDangerZones()
-    {
-        GameManager.Instance.allPlayers[0].dangerZone = pitchZones[9];
-        GameManager.Instance.allPlayers[1].dangerZone = pitchZones[10];
-        GameManager.Instance.allPlayers[2].dangerZone = pitchZones[5];
-        GameManager.Instance.allPlayers[3].dangerZone = pitchZones[6];
-        GameManager.Instance.allPlayers[4].dangerZone = pitchZones[26];
-        GameManager.Instance.allPlayers[5].dangerZone = pitchZones[25];
-        GameManager.Instance.allPlayers[6].dangerZone = pitchZones[30];
-        GameManager.Instance.allPlayers[7].dangerZone = pitchZones[29];
+        GameManager.Instance.allPlayers[6].homeZone = pitchZones[27];
+        GameManager.Instance.allPlayers[7].homeZone = pitchZones[24];
     }
 
     private void SetPlayerMidZones()
     {
         GameManager.Instance.allPlayers[0].midZone = pitchZones[17];
         GameManager.Instance.allPlayers[1].midZone = pitchZones[18];
-        GameManager.Instance.allPlayers[2].midZone = pitchZones[13];
-        GameManager.Instance.allPlayers[3].midZone = pitchZones[14];
+        GameManager.Instance.allPlayers[2].midZone = pitchZones[12];
+        GameManager.Instance.allPlayers[3].midZone = pitchZones[15];
         GameManager.Instance.allPlayers[4].midZone = pitchZones[18];
         GameManager.Instance.allPlayers[5].midZone = pitchZones[17];
-        GameManager.Instance.allPlayers[6].midZone = pitchZones[22];
-        GameManager.Instance.allPlayers[7].midZone = pitchZones[21];
+        GameManager.Instance.allPlayers[6].midZone = pitchZones[23];
+        GameManager.Instance.allPlayers[7].midZone = pitchZones[20];
+    }
+    
+    private void SetPlayerDangerZones()
+    {
+        GameManager.Instance.allPlayers[0].dangerZone = pitchZones[9];
+        GameManager.Instance.allPlayers[1].dangerZone = pitchZones[10];
+        GameManager.Instance.allPlayers[2].dangerZone = pitchZones[4];
+        GameManager.Instance.allPlayers[3].dangerZone = pitchZones[7];
+        GameManager.Instance.allPlayers[4].dangerZone = pitchZones[26];
+        GameManager.Instance.allPlayers[5].dangerZone = pitchZones[25];
+        GameManager.Instance.allPlayers[6].dangerZone = pitchZones[31];
+        GameManager.Instance.allPlayers[7].dangerZone = pitchZones[28];
+    }
+
+    private void SetPlayerAttackZones()
+    {
+        GameManager.Instance.allPlayers[0].attackZone = pitchZones[28];
+        GameManager.Instance.allPlayers[1].attackZone = pitchZones[31];
+        GameManager.Instance.allPlayers[2].attackZone = pitchZones[21];
+        GameManager.Instance.allPlayers[3].attackZone = pitchZones[22];
+        GameManager.Instance.allPlayers[4].attackZone = pitchZones[7];
+        GameManager.Instance.allPlayers[5].attackZone = pitchZones[4];
+        GameManager.Instance.allPlayers[6].attackZone = pitchZones[14];
+        GameManager.Instance.allPlayers[7].attackZone = pitchZones[13];
     }
 }
